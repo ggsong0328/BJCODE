c282f4433c987ff98e7f21dd37cf8b4eabed2d5a
Song <citrus0328@naver.com>
2021-01-03 20:29:57 +0900

    BOJ - 2577

---
+++ 2577.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int arr[10];
+int main() {
+	int A = 0;
+	int B = 0;
+	int C = 0;
+	int N = A * B * C;
+	int TN = N;
+	cin >> A >> B >> C;
+	int digit = 0;
+	while (N != 0) {
+		N = N / 10;
+		++digit;
+	}
+
+	for (int i = (digit-1); i >= 0; i--) {
+		arr[i] = TN % 10;
+		TN = TN / 10;
+	}
+	
+	for (int i = 0; i < 10; i++) {
+		int count = 0;
+		for (int j = 0; j < digit; j++) {
+			if (i == arr[j])
+				count++;
+		}
+		cout << count << endl;
+	}
+
+	return 0;
+}
